cab32be830561ba114ee4859889222996c448115
Andrew Fakhry <andrew.fakhry@outlook.com>
2018-01-30 15:09:39 -0700

    Add license file, clean up code and change frame picture

--- controls.js
+++ controls.js
@@ -1,29 +1,14 @@
+// Start only when DOM is loaded
 document.addEventListener("DOMContentLoaded", function() {
+  // Range and color data attributes
   const inputs = document.querySelectorAll('input');
-
-function updateControls() {
-  var suffix;
-  (this.type == "range") ? suffix = "px" : suffix = "";
-  document.documentElement.style.setProperty("--"+this.name,this.value+suffix);
-    console.log(this.value+suffix);
-}
-
+  // Modifies style variables
+  function updateControls() {
+    var suffix;
+    (this.type == "range") ? suffix = "px" : suffix = "";
+    document.documentElement.style.setProperty("--"+this.name,this.value+suffix);
+      console.log(this.value+suffix);
+  }
+  // Run updateControls function when changes happen
   inputs.forEach(input => input.addEventListener('change', updateControls));
 });
-
-// const inputs = document.querySelectorAll('input');
-// console.log(inputs);
-// // function adjustPicture() {
-// //   console.log("inputs");
-// // }
-//
-// inputs.forEach(function(evt) {
-//   this.addEventListener('mouseover', function () {
-//     console.log("this");
-//   });
-// });
-//
-//
-// // addEventListener('change', function adjustPicture() {
-// //   console.log("inputs");
-// // }));
